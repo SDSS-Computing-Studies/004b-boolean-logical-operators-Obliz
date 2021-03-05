1de0bba7a80796cd14c231995b1c30897b493eae
Obliz <owent304@deltalearns.ca>
2021-03-05 09:51:10 -0800

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -26,3 +26,42 @@ Enter an integer=>4
 Enter an integer=>2
 2,4,5 do not form a Pythagorean triple
 """
+num1=int(input("Enter the first integar: "))
+num2=int(input("Enter the second integar: "))
+num4=int(input("Enter the third integar: "))
+if num1>num2> and num1>num3 and num2>num3:
+
+    gNum=num1
+    nNum=num2
+    sNum=num3
+elif num1>num2 and num1>num3 and num3>num2:
+
+    gNum=num1
+    nNum=num2
+    sNum=num3
+elif num2>num1 and num2>num3 and num1>num3:
+
+    gNum=num2
+    nNum=num1
+    sNum=num3
+elif num2>num1 and num2>num3 and num3>num1:
+
+    gNum=num2
+    nNum=num3
+    sNum=num1
+elif num3>num1 and num3>num2 and num1>num2:
+
+    
+    gNum=num3
+    nNum=num1
+    sNum=num2
+elif num3>num1 and num3>num2 and num2>num1:
+    gNum=num3
+    nNum=num2
+    sNum=num1
+
+if (nNum-num**2 + sNum**2)==(gNum**2):
+    print(str(sNum)+ ","+ str(nNum)+ ","+ str(gNum)+ :"make the pythogorean triple" )
+
+else:
+     print(str(sNum)+ ","+ str(nNum)+ ","+ str(gNum)+ :"Do not make it" )
