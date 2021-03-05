9aab44b9f66c8d73a9ee24ba3a874050c03d82d1
Obliz <owent304@deltalearns.ca>
2021-03-05 09:12:05 -0800

    done

--- problem2.py
+++ problem2.py
@@ -23,3 +23,16 @@ Enter a number: 4
 Enter another number: 25
 4 is not a factor of 25
 """
+number1=int(input("Enter an integar "))
+number2=int(input("Enter another "))
+if (number1>number2)==True:
+    gNum=number1
+    lnum=number2
+else:
+    gNum=number2
+    lNum=number1
+value=gNum%lnum
+if value==0:
+    print(str(lNum) + "is a factor of " + str(tgNum))
+else:
+    print(str(lNum)+ "is not a factor of " + str(gNum))
